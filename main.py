e4dcc6c3bb1ee4a7c2303389fe8e9e2c9afc4d61
GIT Admin <gitnickprogramming@gmail.com>
2020-03-14 14:20:28 -0600

    Loads images but error when trying to run kNN model

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from data import simpledatasetloader
-from Process import simplepreprocessor
+from dataset.simpledatasetloader import SimpleDatasetLoader
 from imutils import paths
 import argparse
 
+from preprocessing.simplepreprocessor import SimplePreprocessor
+
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True, help="kNN_Classifier/data/animals/")
+
+ap.add_argument("-d", "--dataset", required=True, help="../K-Nearest-Neighbor-classifier-/dataset/animals/")
 ap.add_argument("-k", "-- neighbors", type=int, default=1, help="#of nearest neighbors for classification")
 ap.add_argument("-j", "--jobs", type=int, default=-1,
                 help="# of jobs for k-NN distance (-1 uses all available cores)")
 args = vars(ap.parse_args())
 
 print("[INFO] loading images...")
-imagePaths = list(paths.list_images(args["dataset"]))
+imagePaths = list(paths.list_images('../K-Nearest-Neighbor-classifier-/dataset/animals/'))
 
-sp = simplepreprocessor.SimplePreprocessor(32, 32)
-sdl = simpledatasetloader.SimpleDatasetLoader(preprocessors=[sp])
+sp = SimplePreprocessor(32, 32)
+sdl = SimpleDatasetLoader(preprocessors=[sp])
 (data, labels) = sdl.load(imagePaths, verbose=500)
 data = data.reshape((data.shape[0], 3072))
 
@@ -27,10 +29,10 @@ print("[INFO] features matrix: {:.1f}MB".format(data.nbytes / (1024 * 1000.0)))
 le = LabelEncoder()
 labels = le.fit_transform(labels)
 
-(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=0)
 
 print("[INFO] evaluating k-NN classifier...")
-model = KNeighborsClassifier(n_neighbors=args["neighbors"], n_jobs=args["jobs"])
+model = KNeighborsClassifier(n_neighbors=args["neighbors"], n_jobs=["jobs"])
 model.fit(trainX, trainY)
 print(classification_report(testY, model.predict(testX), target_names=le.classes_))
 
